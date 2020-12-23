00c53fc4349e4eab4d0381edf794f36efacab900
Glitch (hello-express) <none>
2020-12-23 02:25:23 +0000

    🍄🦂 Checkpoint ./public/script.js:1213050/1727

--- public/script.js
+++ public/script.js
@@ -7,14 +7,39 @@ function setTemp(value) {
   document.getElementById('tempbox').value = value;
 }
 
-function translateInto() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function translateInto() {
   const toTranslate = document.getElementById('englishbox').value;
-  console.log(toTranslate);
   
   setTemp(toTranslate);
   
   const transArray = toTranslate.split(" ");
   
+  const tempArray = [];
   
+  await transArray.reduce(async (memo, i) => {
+	  await memo;
+	  await sleep(10 - i);
+    var word = i;
+    
+    // case changes
+    if (word.toLowerCase() == "me") word = "I";
+    
+    // possessive pronouns
+    if (word.toLowerCase() == "my") word = "I's";
+    if (word.toLowerCase() == "your") word = "you's";
+    if (word.toLowerCase() == "his") word = "he's";
+    if (word.toLowerCase() == "her") word = "she's";
+    if (word.toLowerCase() == "their") word = "they's";
+    if (word.toLowerCase() == "our") word = "we's";
+    
+    tempArray.push(word);
+    console.log(i + " => " + word);
+}, undefined);
+  
+  setTemp(tempArray.join(' '));
   
 }
